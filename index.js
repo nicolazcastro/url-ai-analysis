94494851afa6c0c96277641ca3c0cff96c2929e1
nicolazcastro <nicolazcastro@gmail.com>
2024-03-07 16:35:49 -0300

    in progress

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// index.js del frontend
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const httpProxy = require('http-proxy');
-const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 
@@ -14,11 +11,9 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// Configuración del proxy
 const proxy = httpProxy.createProxyServer();
-const userServiceUrl = process.env.USER_SERVICE_URL || 'http://localhost:3001'; // URL del microservicio de usuarios
+const userServiceUrl = process.env.USER_SERVICE_URL || 'http://localhost:3001'; 
 
-// Redirigir las solicitudes relacionadas con usuarios al microservicio correspondiente
 app.use('/users', (req, res) => {
     proxy.web(req, res, { target: userServiceUrl });
 });
