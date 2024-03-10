b7328ec046ed8426fa27f0959be3ce96052630cd
nicolazcastro <nicolazcastro@gmail.com>
2024-03-10 18:24:46 -0300

    -

--- deploy.sh
+++ deploy.sh
@@ -54,6 +54,15 @@ for branch in "${branches[@]}"; do
     # Change directory to the branch directory
     cd "$branch_dir" || exit
 
+    # Modify launch.json
+    sed -i '' "s/\"name\": \"Launch Web Server\"/\"name\": \"Launch $branch Web Server\"/" .vscode/launch.json
+
+    # Add localRoot and remoteRoot properties
+    sed -i '' '/"type": "node",/,/"env": {/ {
+        /"skipFiles": \[/a\
+        \t"localRoot": "${workspaceFolder}",\
+        \t"remoteRoot": "/path/to/deployed/folder",'}' .vscode/launch.json
+
     echo ""
     echo "copying files $branch to $branch_dir"
     rsync -av --exclude='*.env' --exclude='deploy.sh' --exclude='.git' --exclude='.gitignore' --exclude='deploy' --exclude='node_modules' --exclude='package-lock.json' ../../ .
