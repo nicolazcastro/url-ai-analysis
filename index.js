6a034b4cf914504629a3a4901fdea8a6af91bd6b
nicolazcastro <nicolazcastro@gmail.com>
2024-03-11 12:47:51 -0300

    -

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// index.js del frontend
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const httpProxy = require('http-proxy');
+const path = require('path');
 
 dotenv.config();
 
@@ -12,22 +10,6 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
-
-// Configuración del proxy
-const proxy = httpProxy.createProxyServer();
-const userServiceUrl = process.env.USER_SERVICE_URL || 'http://localhost:3001'; // user microservice URL
-const urlAnalysisServiceUrl = process.env.ANALYSIS_SERVICE_URL || 'http://localhost:3002'; // analysis microservice URL
-
-// Redirigir las solicitudes relacionadas con usuarios al microservicio correspondiente
-app.use('/user', (req, res) => {
-    proxy.web(req, res, { target: userServiceUrl });
-});
-
-app.use('/analysis', (req, res) => {
-    proxy.web(req, res, { target: urlAnalysisServiceUrl });
-});
-
-app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.listen(port, () => {
     console.log(`Frontend server running at http://localhost:${port}`);
--- routes/routes.js
+++ routes/routes.js
@@ -1,29 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const httpProxy = require('http-proxy');
-const dotenv = require('dotenv');
+const path = require('path');
 
-dotenv.config();
+// Serve static files from the /public directory
+router.use(express.static(path.join(__dirname, 'public')));
 
-const proxy = httpProxy.createProxyServer();
-const userServiceUrl = process.env.USER_SERVICE_URL || 'http://localhost:3001'; // user microservice URL
-const urlAnalysisServiceUrl = process.env.ANALYSIS_SERVICE_URL || 'http://localhost:3002'; // analysis microservice URL
-// Define other microservice URLs as needed
-
-// Route definitions
-router.post('/user/register', (req, res) => {
-    proxy.web(req, res, { target: userServiceUrl });
+// Route for serving favicon
+router.get('/favicon.ico', (req, res) => {
+    // Return a 204 No Content response for favicon requests
+    res.status(204).end();
 });
-
-router.post('/user/login', (req, res) => {
-    proxy.web(req, res, { target: userServiceUrl });
-});
-
-router.post('/url-analysis/analyze', (req, res) => {
-    // Proxy request to URL analysis microservice
-    proxy.web(req, res, { target: urlAnalysisServiceUrl }); // URL of the URL analysis microservice
-});
-
-// Add more routes for other microservices as needed
 
 module.exports = router;
